672865a33e56ee1a45fdf91192a7c527d6404d36
Robert Nolan <robertnolan208@gmail.com>
2026-04-13 20:15:09 +0100

    home page layout done with Student Number, Dark Mode and Favourites Icon, Search Bar, and Dummy Movie Titles for now

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+// Icons
+import { addIcons } from 'ionicons';
+import { moonOutline, heartOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
 })
+
+// Home Page Layout
 export class HomePage {
-  constructor() {}
+
+  searchTerm: string = '';
+
+  dummyMovies = [
+    { title: 'Movie 1', description: 'This is a short description...' },
+    { title: 'Movie 2', description: 'Another movie description...' },
+    { title: 'Movie 3', description: 'More placeholder text...' },
+    { title: 'Movie 4', description: 'More placeholder text...' }
+  ];
+
+  constructor() {
+    addIcons({
+      'moon-outline': moonOutline,
+      'heart-outline': heartOutline
+    });
+  }
+
+
+  /* For applying Dark Mode Toggle */
+  toggleDarkMode() {
+    document.body.classList.toggle('dark');
+  }
+
+  /* For Search Function */
+  onSearch() {
+    console.log("Searching:", this.searchTerm);
+  }
+
 }
+
+
+ 
+
