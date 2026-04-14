b0df2e3c4e3c1ee5fda0696b1d5b92c6a14f8571
Robert Nolan <robertnolan208@gmail.com>
2026-04-14 18:53:52 +0100

    movie details page - layout improved for screen sizes, add / remove fav button added

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -9,7 +9,7 @@ import { ApiService } from '../../services/api.service';
 
 // Icons
 import { addIcons } from 'ionicons';
-import { moonOutline, heartOutline } from 'ionicons/icons';
+import { moonOutline, heartOutline, heart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-movie-details',
@@ -30,7 +30,8 @@ export class MovieDetailsPage implements OnInit {
   ) {
     addIcons({
       'moon-outline': moonOutline,
-      'heart-outline': heartOutline
+      'heart-outline': heartOutline,
+      'heart': heart
     });
   }
 
@@ -40,11 +41,14 @@ export class MovieDetailsPage implements OnInit {
     if (id) {
       this.apiService.getMovieDetails(id).subscribe((data: any) => {
         this.movie = data;
+
+        const favourites = JSON.parse(localStorage.getItem('favourites') || '[]');
+        this.isFavourite = favourites.some((m: any) => m.id === this.movie.id);
       });
 
       this.apiService.getMovieCredits(id).subscribe((data: any) => {
-        this.cast = data.cast.slice(0, 5); // top 5 cast
-        this.crew = data.crew.slice(0, 5); // top 5 crew
+        this.cast = data.cast.slice(0, 10); // top 10 cast
+        this.crew = data.crew.slice(0, 10); // top 10 crew
       });
     }
   }
@@ -52,5 +56,24 @@ export class MovieDetailsPage implements OnInit {
   toggleDarkMode() {
     document.body.classList.toggle('dark');
   }
+
+  isFavourite: boolean = false;
+
+  toggleFavourite() {
+    let favourites = JSON.parse(localStorage.getItem('favourites') || '[]');
+
+    if (this.isFavourite) {
+      // Remove
+      favourites = favourites.filter((m: any) => m.id !== this.movie.id);
+      this.isFavourite = false;
+    } else {
+      // Add
+      favourites.push(this.movie);
+      this.isFavourite = true;
+    }
+
+    localStorage.setItem('favourites', JSON.stringify(favourites));
+  }
+
 
 }
